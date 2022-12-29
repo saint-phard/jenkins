5d2ff0c5966f29a4c823ab46cef2c67378e7ff7d
phard <phard911@gmail.com>
2022-12-29 18:57:11 +0000

    parameterized image name

--- script.groovy
+++ script.groovy
@@ -1,14 +1,14 @@
 def buildJar() {
     echo 'building the application with maven'
-    sh 'mvn package'
+    sh 'mvn clean package'
 }
 
 def buildImage() {
     echo 'building the docker image'
     withCredentials([usernamePassword(credentialsId: 'my-dockerhub-credentials', usernameVariable: 'username', passwordVariable: 'password')]) {
-        sh 'docker build -t phard/the-app:maven-2.0 .'
+        sh 'docker build -t phard/the-app:${IMAGE_NAME} .'
         sh "echo $password | docker login -u $username --password-stdin"
-        sh 'docker push phard/the-app:maven-2.0'
+        sh 'docker push phard/the-app:${IMAGE_NAME}'
     }
 }
 
